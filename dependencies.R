d395af6f930eab5d132868ef3636935ebd92f5f7
Yogesh Khandelwal <yogesh1612@users.noreply.github.com>
2021-04-02 15:02:25 +0530

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -2,3 +2,4 @@
 if (!require(shiny)) {install.packages("shiny")};  suppressWarnings(library(shiny))
 if (!require(dplyr)) {install.packages("dplyr")};  suppressWarnings(library(dplyr))
 if (!require(pixels)) {install.packages("pixels")};  suppressWarnings(library(pixels))
+if (!require(markdown)) {install.packages("markdown")};  suppressWarnings(library(markdown))
